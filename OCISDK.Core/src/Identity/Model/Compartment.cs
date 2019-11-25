785894c5ea4f69adc3912ab27744784cd256c809
kfurusawa <k.furusawa@atomitech.jp>
2019-11-25 10:48:47 +0900

    correction compartment API
    * correction compartment detail model parameter

--- OCISDK.Core/src/Identity/Model/Compartment.cs
+++ OCISDK.Core/src/Identity/Model/Compartment.cs
@@ -1,22 +1,21 @@
-﻿/// <summary>
-/// Compartment Reference
-/// A collection of related resources. Compartments are a fundamental component of 
-/// Oracle Cloud Infrastructure for organizing and isolating your cloud resources. 
-/// You use them to clearly separate resources for the purposes of measuring usage 
-/// and billing, access (through the use of IAM Service policies), and isolation 
-/// (separating the resources for one project or business unit from another). A common 
-/// approach is to create a compartment for each major part of your organization. 
-/// For more information, see Overview of the IAM Service and also Setting Up Your 
-/// Tenancy.
-/// 
-/// author: koutaro furusawa
-/// </summary>
-
-
+﻿
 using System.Collections.Generic;
 
 namespace OCISDK.Core.src.Identity.Model
 {
+    /// <summary>
+    /// Compartment Reference
+    /// A collection of related resources. Compartments are a fundamental component of 
+    /// Oracle Cloud Infrastructure for organizing and isolating your cloud resources. 
+    /// You use them to clearly separate resources for the purposes of measuring usage 
+    /// and billing, access (through the use of IAM Service policies), and isolation 
+    /// (separating the resources for one project or business unit from another). A common 
+    /// approach is to create a compartment for each major part of your organization. 
+    /// For more information, see Overview of the IAM Service and also Setting Up Your 
+    /// Tenancy.
+    /// 
+    /// author: koutaro furusawa
+    /// </summary>
     public class Compartment
     {
         public string Id { get; set; }
@@ -31,9 +30,9 @@ namespace OCISDK.Core.src.Identity.Model
         
         public string LifecycleState { get; set; }
         
-        public int InactiveStatus { get; set; }
+        public int? InactiveStatus { get; set; }
         
-        public bool IsAccessible { get; set; }
+        public bool? IsAccessible { get; set; }
         
         public IDictionary<string, string> FreeformTags { get; set; }
         
