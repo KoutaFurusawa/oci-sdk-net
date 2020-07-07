69b118a2a90614e91817fccb63e2f82fd2ddef12
k.frusawa <k.furusawa@atomitech.jp>
2020-07-07 12:01:08 +0900

    objectStorageDirectoryInfo Bug fix
    objectStorageDirectoryInfo Bug fix

--- Example/ObjectStorageDirectoryInfoExample.cs
+++ Example/ObjectStorageDirectoryInfoExample.cs
@@ -1,6 +1,7 @@
 ﻿using OCISDK.Core;
 using OCISDK.Core.Common;
 using OCISDK.Core.Identity;
+using OCISDK.Core.Identity.Model;
 using OCISDK.Core.Identity.Request;
 using OCISDK.Core.ObjectStorage;
 using OCISDK.Core.ObjectStorage.IO;
@@ -35,9 +36,11 @@ namespace Example
                 CompartmentIdInSubtree = true,
                 AccessLevel = ListCompartmentRequest.AccessLevels.ACCESSIBLE
             };
-            var compartments = identityClient.ListCompartment(listCompartmentRequest).Items;
+            /*var compartments = identityClient.ListCompartment(listCompartmentRequest).Items;
             // root(tenant) add
-            compartments.Add(new OCISDK.Core.Identity.Model.Compartment { Id = config.TenancyId, Name = "root" });
+            compartments.Add(new OCISDK.Core.Identity.Model.Compartment { Id = config.TenancyId, Name = "root" });*/
+            List<Compartment> compartments = new List<Compartment>();
+            compartments.Add(identityClient.GetCompartment(new GetCompartmentRequest { CompartmentId = "ocid1.compartment.oc1..aaaaaaaaibq365qp5tws2vhuvd3vhzwphtwx4zlhybjrrvwarjrmeud4jiya" }).Compartment);
 
             foreach (var compartment in compartments)
             {
@@ -62,17 +65,45 @@ namespace Example
                         Console.WriteLine($"\t|- {file.Name}");
                     }
 
-                    // all directories
-                    var topDirs = directoryInfo.EnumerateDirectories("*", System.IO.SearchOption.AllDirectories);
+                    // top directories and files
+                    var topDirs = directoryInfo.EnumerateDirectories("*", System.IO.SearchOption.TopDirectoryOnly);
 
                     foreach (var dir in topDirs)
                     {
-                        Console.WriteLine($"\t|- Dir name:{dir.Name}");
+                        Console.WriteLine($"\t|- {dir.Name}");
                         files = dir.EnumerateFiles("*", System.IO.SearchOption.TopDirectoryOnly);
 
                         foreach (var file in files)
                         {
                             Console.WriteLine($"\t|\t|- {file.Name}");
+                        }
+
+                        // sub directories and files
+                        var subDirInfo = new ObjectStorageDirectoryInfo(client, nameSpaceName, bukcet.Name, dir.Name+"/");
+                        var subDirs = subDirInfo.EnumerateDirectories("*", System.IO.SearchOption.TopDirectoryOnly);
+
+                        foreach (var sub in subDirs)
+                        {
+                            Console.WriteLine($"\t|\t|- {sub.Name}");
+                            files = sub.EnumerateFiles("*", System.IO.SearchOption.TopDirectoryOnly);
+
+                            foreach (var file in files)
+                            {
+                                Console.WriteLine($"\t|\t|\t|- {file.Name}");
+                            }
+                        }
+                    }
+
+                    foreach (var dir in topDirs)
+                    {
+                        // prefix directories
+                        Console.WriteLine("prefix-----[" + dir.Name + "]");
+                        var preDir = new ObjectStorageDirectoryInfo(client, nameSpaceName, bukcet.Name, dir.Name);
+                        var preFiles = preDir.EnumerateFiles("*", System.IO.SearchOption.TopDirectoryOnly);
+
+                        foreach (var file in preFiles)
+                        {
+                            Console.WriteLine($"\t|- {file.Name}");
                         }
                     }
                 }
